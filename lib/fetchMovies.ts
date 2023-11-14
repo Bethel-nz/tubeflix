bd1049c273057ea461044925f9f85c07e7624e11
Nzekea Bethel <nzekeabethel@outlook.com>
2023-11-14 21:31:42 +0100

    fix: incorrect backend link in fetch fn

--- lib/fetchMovies.ts
+++ lib/fetchMovies.ts
@@ -2,9 +2,12 @@ import { MoviesData } from '@/types/types';
 import { BASE_URL } from '@/constants/constants';
 
 export default async function fetchMovies(page: number = 1) {
-	const res = await fetch(`${BASE_URL}/api/movies?page=${page}`, {
-		cache: 'no-store',
-	});
+	const res = await fetch(
+		`https://${process.env.VERCEL_URL}/api/movies?page=${page}`,
+		{
+			cache: 'no-store',
+		}
+	);
 	const movies: MoviesData = await res.json();
 	return movies;
 }
--- lib/getResult.ts
+++ lib/getResult.ts
@@ -1,8 +1,11 @@
 import { BASE_URL } from '@/constants/constants';
 export default async function getResult(q: string, page: number = 1) {
-	const res = await fetch(`${BASE_URL}/api/Search?q=${q}&page=${page}`, {
-		cache: 'no-store',
-	});
+	const res = await fetch(
+		`https://${process.env.VERCEL_URL}/api/Search?q=${q}&page=${page}`,
+		{
+			cache: 'no-store',
+		}
+	);
 	const movies = await res.json();
 	return movies;
 }
