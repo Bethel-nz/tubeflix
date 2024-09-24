702e7990de78fc7e663a9cccce793dd9165ef98a
ren. <nzekeabethel@outlook.com>
2024-09-25 00:04:03 +0100

    Update fetchMovies.ts

--- lib/fetchMovies.ts
+++ lib/fetchMovies.ts
@@ -1,7 +1,8 @@
 import { MoviesDataResponse } from '@/types/types';
+import { BASE_URL } from '@/constants';
 
 export default async function fetchMovies(page: number = 1) {
-  const res = await fetch(`https://${process.env.VERCEL_URL}/api/movies?page=${page}`, {
+  const res = await fetch(`BASE_URL}/api/movies?page=${page}`, {
     cache: 'no-store',
   });
   const movies: MoviesDataResponse = await res.json();
