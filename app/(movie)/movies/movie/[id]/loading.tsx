67ecb6ad7efadbc0ebbde288efe2ed29ebe94083
bethel. <71951344+Bethel-nz@users.noreply.github.com>
2024-09-27 07:37:46 +0100

    feat: revamped loading page

--- app/(movie)/movies/movie/[id]/loading.tsx
+++ app/(movie)/movies/movie/[id]/loading.tsx
@@ -1,32 +1,11 @@
+import MovieLoading from '@/components/shared/Skeleton/MovieLoading';
+
 const Loading = () => {
-	return (
-		<div>
-			<div className='mx-auto relative'>
-				<div className='flex flex-col lg:flex-row items-start space-y-4 lg:space-y-0 gap-x-4'>
-					<div className='lg:w-[60rem]'>
-						<div className='rounded-md bg-gray-300 h-[60rem]'></div>
-					</div>
-					<div className='lg:w-[40%] bg-black/90 rounded-md md:rounded-l-none md:rounded-r-md pl-4 p-8 shadow-xl md:absolute md:bottom-0'>
-						<h1 className='text-3xl font-bold mt-4 bg-gray-300 w-[80%] h-8'></h1>
-						<p className='text-lg mt-2 bg-gray-300 w-[100%] h-4'></p>
-						<div className='flex flex-wrap gap-4 mt-4'>
-							<div className='bg-gray-300 w-32 h-8'></div>
-							<div className='bg-gray-300 w-32 h-8'></div>
-							<div className='bg-gray-300 w-32 h-8'></div>
-						</div>
-						<div className='mt-4'>
-							<h2 className='font-bold text-2xl'>Trailers:</h2>
-							<ul>
-								<li className='bg-gray-300 w-32 h-6'></li>
-								<li className='bg-gray-300 w-32 h-6'></li>
-								<li className='bg-gray-300 w-32 h-6'></li>
-							</ul>
-						</div>
-					</div>
-				</div>
-			</div>
-		</div>
-	);
+  return (
+    <div className='h-screen w-screen flex items-center justify-center bg-black/50'>
+      <MovieLoading />
+    </div>
+  );
 };
 
 export default Loading;
